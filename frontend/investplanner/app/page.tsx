c251153424bff7636708e31cd4e3ebb14fe9e3a6
karthick7204 <karthicklimited1234@gmail.com>
2026-04-17 17:15:04 +0530

    changed the folder struct

--- frontend/investplanner/app/page.tsx
+++ frontend/investplanner/app/page.tsx
@@ -1,15 +1,12 @@
 import Image from "next/image";
 import Sidebar from "./sidebar/sidebar";
-import  DashboardPage from "./dashboard/DashBoard";
+import  DashboardPage from "./dashboard/DashBoard"
 import Header from "./header/header";
 import LoginPage from "./auth/login/page";
 import LandingPage from "./landingpage/page";
 export default function Home() {
   return (
-    
        <LandingPage />
-        
- 
   )
 
 }
