b9df018d9dfdb95ff7618911070d430befd75d64
karthick7204 <karthicklimited1234@gmail.com>
2026-04-17 17:18:27 +0530

    changed folder struct

--- frontend/investplanner/app/page.tsx
+++ frontend/investplanner/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Sidebar from "./sidebar/sidebar";
-import  DashboardPage from "./dashboard/DashBoard"
+import  DashboardPage from "./dashboard/DashBoard";
 import Header from "./header/header";
 import LoginPage from "./auth/login/page";
 import LandingPage from "./landingpage/page";
